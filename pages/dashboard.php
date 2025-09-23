0163666f270205ab5ba33938789a279160c44b46
Omwitsa <omwitsawilson@yahoo.com>
2025-09-23 21:27:58 +0300

    Save chnages

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -1,63 +1,67 @@
 <div class="container-fluid">
-   <div class="row">
-      <div class="main-header">
-         <h1>Danish Refugee Council SDACKED Program</h1>
+   <div class="row about_drc">
+      <div class="row">
+         <div class="main-header">
+            <h1>Danish Refugee Council SDACKED Program</h1>
+         </div>
+      </div>
+      <div class="row">
+         <div class="col-sm-12"><h5>Who is the Danish Refugee Council?</h5></div>
+      </div>
+      <div class="row">
+         <div class="col-md-6 col-sm-12">
+            <p>Founded in 1956, the Danish Refugee Council (DRC) is a leading international NGO and one of the few 
+            with specific expertise in forced displacement. Active in 40 countries, with 9,000 employees and
+            supported by 7,500 volunteers, DRC protects, advocates, and builds sustainable futures for refugees
+            and other displacement-affected people and communities. DRC works during displacement at all
+            stages: in the acute crisis, in displacement, when settling and integrating in a new place, or upon
+            return. DRC provides protection and life-saving humanitarian assistance, supporting displaced persons
+            in becoming self-reliant and included in hosting societies. DRC works with civil society and responsible
+            authorities to promote the protection of refugee rights and peaceful coexistence.
+            DRC has been operational in Kenya since 2005 and is one of the United Nations High Commissioner for
+            Refugees (UNHCR) largest implementing partners in refugee response. The organization is present in
+            Garissa (Dadaab & Garissa), Turkana (Kakuma, Kalobeyei & Lodwar), Lamu, Isiolo, Marsabit, Nairobi,
+            and Mandera counties.</p>
+         </div>
+         <div class="col-md-6 col-sm-12">
+            <img id="img_sdacked" src="../assets/images/sdacked.jpg" alt="SDACKED">
+         </div>
       </div>
    </div>
-   <div class="row">
-      <div class="col-sm-12"><h4>Who is the Danish Refugee Council?</h4></div>
-   </div>
-   <!-- 4-blocks row start -->
-   <div class="row content1">
-      <div class="col-md-6 col-sm-12">
-         <p>Founded in 1956, the Danish Refugee Council (DRC) is a leading international NGO and one of the few <br>
-         with specific expertise in forced displacement. Active in 40 countries, with 9,000 employees and <br>
-         supported by 7,500 volunteers, DRC protects, advocates, and builds sustainable futures for refugees <br>
-         and other displacement-affected people and communities. DRC works during displacement at all <br>
-         stages: in the acute crisis, in displacement, when settling and integrating in a new place, or upon <br>
-         return. DRC provides protection and life-saving humanitarian assistance, supporting displaced persons <br>
-         in becoming self-reliant and included in hosting societies. DRC works with civil society and responsible <br>
-         authorities to promote the protection of refugee rights and peaceful coexistence. <br>
-         DRC has been operational in Kenya since 2005 and is one of the United Nations High Commissioner for <br>
-         Refugees (UNHCR) largest implementing partners in refugee response. The organization is present in <br>
-         Garissa (Dadaab & Garissa), Turkana (Kakuma, Kalobeyei & Lodwar), Lamu, Isiolo, Marsabit, Nairobi, <br>
-         and Mandera counties.</p>
+  
+   <div class="row drc_programs">
+      <div class="row">
+         <div class="col-sm-12"><h1>Featured Programs</h1></div>
       </div>
-      <div class="col-md-6 col-sm-12">
-         <img id="img_sdacked" src="../assets/images/sdacked.jpg" alt="SDACKED">
+      
+      <div class="row">
+         <div class="col-md-4 col-sm-12">
+            <img src="../assets/images/product1.png" alt="Product">
+         </div>
+         <div class="col-md-4 col-sm-12">
+            <img src="../assets/images/product2.png" alt="Product">
+         </div>
+         <div class="col-md-4 col-sm-12">
+            <img src="../assets/images/product3.png" alt="Product">
+         </div>
       </div>
    </div>
-   <!-- 4-blocks row end -->
 
-   <!-- 2-1 block start -->
-   <div class="row">
-      <div class="col-sm-12"><h4>Featured Programs</h4></div>
-   </div>
-   <div class="row">
-      <div class="col-md-4 col-sm-12">
-         <img class="img_product" src="../assets/images/product1.png" alt="Product">
-      </div>
-      <div class="col-md-4 col-sm-12">
-         <img class="img_product" src="../assets/images/product2.png" alt="Product">
-      </div>
-      <div class="col-md-4 col-sm-12">
-         <img class="img_product" src="../assets/images/product3.png" alt="Product">
-      </div>
-   </div>
-   <!-- 2-1 block end -->
-   <div class="row">
+   <div class="row drc_contacts">
       <div class="col-md-6 col-sm-12">
-         <h1>Contact Us</h1>
-         <p>Interested in working together? Fill out some info and we will be in touch shortly. We can’t wait to hear from you!</p>
+         <div class="row">
+            <div class="col-sm-12"><h1>Contact Us</h1></div>
+         </div>
+         
+         <div class="row">
+            <div class="col-sm-12">
+               <p>Interested in working together? Fill out some info and we will be in touch shortly. We can’t wait to hear from you!</p>
+            </div>
+         </div>
       </div>
+
       <div class="col-md-6 col-sm-12">
+         
       </div>
    </div>
-   <div class="row">
-      <div class="col-md-6 col-sm-12">
-         <h1>Your Site Title</h1>
-      </div>
-      <div class="col-md-6 col-sm-12">
-      </div>
-   </div><br><br>
 </div>
--- services/database_connection.php
+++ services/database_connection.php
@@ -1,6 +1,6 @@
 <?php
 try {
-     $connect = new PDO('mysql:host=localhost; dbname=slmd', 'ingress', 'ingress');
+     $connect = new PDO('mysql:host=localhost; dbname=slmd', 'vnc', 'Newtech@2022');
 } catch (PDOException $e) {
     print "Error!: " . $e->getMessage() . "<br/>";
     die();

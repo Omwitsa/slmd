b42b2a1d085cf22e51480452cd0e327203ce12ca
Omwitsa <omwitsawilson@yahoo.com>
2025-09-24 10:40:43 +0300

    Save changes

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -1,27 +1,37 @@
-<div class="container-fluid">
+<div class="container-fluid"><br>
+   <div class="row drc_logo">
+      <div class="col-md-2 col-sm-12">
+         <img src="../assets/images/logo1.png">
+      </div>
+   </div>
+
    <div class="row about_drc">
       <div class="row">
-         <div class="main-header">
-            <h1>Danish Refugee Council SDACKED Program</h1>
-         </div>
-      </div>
-      <div class="row">
-         <div class="col-sm-12"><h5>Who is the Danish Refugee Council?</h5></div>
-      </div>
-      <div class="row">
          <div class="col-md-6 col-sm-12">
-            <p>Founded in 1956, the Danish Refugee Council (DRC) is a leading international NGO and one of the few 
-            with specific expertise in forced displacement. Active in 40 countries, with 9,000 employees and
-            supported by 7,500 volunteers, DRC protects, advocates, and builds sustainable futures for refugees
-            and other displacement-affected people and communities. DRC works during displacement at all
-            stages: in the acute crisis, in displacement, when settling and integrating in a new place, or upon
-            return. DRC provides protection and life-saving humanitarian assistance, supporting displaced persons
-            in becoming self-reliant and included in hosting societies. DRC works with civil society and responsible
-            authorities to promote the protection of refugee rights and peaceful coexistence.
-            DRC has been operational in Kenya since 2005 and is one of the United Nations High Commissioner for
-            Refugees (UNHCR) largest implementing partners in refugee response. The organization is present in
-            Garissa (Dadaab & Garissa), Turkana (Kakuma, Kalobeyei & Lodwar), Lamu, Isiolo, Marsabit, Nairobi,
-            and Mandera counties.</p>
+            <div class="row">
+               <div class="main-header">
+                  <h1>Danish Refugee Council SDACKED Program</h1>
+               </div>
+            </div>
+            <div class="row">
+               <div class="col-sm-12"><h5>Who is the Danish Refugee Council?</h5></div>
+            </div>
+            <div class="row">
+               <div class="col-sm-12">
+                  <p>Founded in 1956, the Danish Refugee Council (DRC) is a leading international NGO and one of the few 
+                  with specific expertise in forced displacement. Active in 40 countries, with 9,000 employees and
+                  supported by 7,500 volunteers, DRC protects, advocates, and builds sustainable futures for refugees
+                  and other displacement-affected people and communities. DRC works during displacement at all
+                  stages: in the acute crisis, in displacement, when settling and integrating in a new place, or upon
+                  return. DRC provides protection and life-saving humanitarian assistance, supporting displaced persons
+                  in becoming self-reliant and included in hosting societies. DRC works with civil society and responsible
+                  authorities to promote the protection of refugee rights and peaceful coexistence.
+                  DRC has been operational in Kenya since 2005 and is one of the United Nations High Commissioner for
+                  Refugees (UNHCR) largest implementing partners in refugee response. The organization is present in
+                  Garissa (Dadaab & Garissa), Turkana (Kakuma, Kalobeyei & Lodwar), Lamu, Isiolo, Marsabit, Nairobi,
+                  and Mandera counties.</p>
+               </div>
+            </div>
          </div>
          <div class="col-md-6 col-sm-12">
             <img id="img_sdacked" src="../assets/images/sdacked.jpg" alt="SDACKED">
